23898c9bbe2d76d59a43c8b747853659fadf140e
zahrasabila <zahra.sabila22@mhs.uinjkt.ac.id>
2024-02-11 22:27:35 +0700

    add id google analytics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRoutes from './router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./router";
 
-import { Provider } from 'react-redux';
-import { store } from './state/store';
+import { Provider } from "react-redux";
+import ReactGA from "react-ga4";
+import { store } from "./state/store";
 
-import './styles/global.css'
+import "./styles/global.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-7DWEM5G3MZ");
+
+ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,4 +20,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
